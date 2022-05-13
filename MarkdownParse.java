156e04ae00aea52998f4eab3cf23e19f2d6def3e
Shoumil Sarkar <32953474+ShoumilSarkar@users.noreply.github.com>
2022-05-13 16:58:59 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,25 +13,30 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
+            int closeBracket = markdown.indexOf("]", Math.max(currentIndex, openBracket));
+            int openParen = markdown.indexOf("(", Math.max(currentIndex, closeBracket));
+            int closeParen = markdown.indexOf(")", Math.max(currentIndex, openParen));
             if(openBracket == -1 && closeBracket == -1 && openParen == -1 && closeParen == -1){break;}
             if(openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1){
-                int max1 = Math.max(openBracket, currentIndex);
+                int max1 = Math.max(openBracket, closeBracket);
                 int max2 = Math.max(openParen, closeParen);
                 currentIndex = Math.max(max1, max2) +1;
             }else{
-                String s = markdown.substring(openParen + 1, closeParen);
-                if(!(s.contains("[")||s.contains("("))){
-                    if((openParen == closeBracket+1)&&(openBracket==0||markdown.charAt(openBracket)!='!')){
-                        toReturn.add(markdown.substring(openParen + 1, closeParen));
+                String s = markdown.substring(openParen+1, closeParen);
+                if(!(s.contains("(") || s.contains("["))){
+                    if(openParen == closeBracket + 1){
+                        try{
+                            if(markdown.charAt(openBracket-1) != '!'){
+                                toReturn.add(s);
+                            }
+                        }catch(IndexOutOfBoundsException e){
+                            toReturn.add(s);
+                        }
                     }
                 }
-                currentIndex = closeParen + 1;
-                
+                currentIndex = closeParen + 1;   
             }
-            
+
         }
         return toReturn;
     }
