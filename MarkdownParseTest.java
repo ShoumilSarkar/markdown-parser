c1e909284fe187540c116f14848e522d08e745d4
Shoumil Sarkar <32953474+ShoumilSarkar@users.noreply.github.com>
2022-05-13 17:03:23 -0700

    Reset MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,51 +1,137 @@
-//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+import static org.junit.Assert.*;
+import org.junit.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.ArrayList;
 
-public class MarkdownParse {
 
-    public static ArrayList<String> getLinks(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then read link upto next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", Math.max(currentIndex, openBracket));
-            int openParen = markdown.indexOf("(", Math.max(currentIndex, closeBracket));
-            int closeParen = markdown.indexOf(")", Math.max(currentIndex, openParen));
-            if(openBracket == -1 && closeBracket == -1 && openParen == -1 && closeParen == -1){break;}
-            if(openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1){
-                int max1 = Math.max(openBracket, closeBracket);
-                int max2 = Math.max(openParen, closeParen);
-                currentIndex = Math.max(max1, max2) +1;
-            }else{
-                String s = markdown.substring(openParen+1, closeParen);
-                if(!(s.contains("(") || s.contains("["))){
-                    if(openParen == closeBracket + 1){
-                        try{
-                            if(markdown.charAt(openBracket-1) != '!'){
-                                toReturn.add(s);
-                            }
-                        }catch(IndexOutOfBoundsException e){
-                            toReturn.add(s);
-                        }
-                    }
-                }
-                currentIndex = closeParen + 1;   
-            }
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+    
+    @Test
+    public void getLinksTestFile() throws IOException{
+        //create list of answer links
+        List<String> linksList = List.of("https://something.com", "some-thing.html");
 
-        }
-        return toReturn;
+        //get the file data, read it as a string and passs it into the markdown parse function
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content),linksList);
+
+    }
+
+    @Test
+    public void getLinksTestFile2old() throws IOException{
+        //create list of answer links
+        List<String> linksList = List.of("hello.com", "https://nyaa.si","hello.com");
+
+        //get the file data, read it as a string and passs it into the markdown parse function
+        Path fileName = Path.of("test-file-2.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content),linksList);
+
     }
 
 
-    public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
+
+
+    //new tests start here
+
+    @Test
+    public void getLinksTestNewFile2() throws IOException{
+        //create list of answer links
+        List<String> linksList = List.of("https://something.com", "some-page.html");
+
+        //get the file data, read it as a string and passs it into the markdown parse function
+        Path fileName = Path.of("test-file2.md");
         String content = Files.readString(fileName);
-        ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        assertEquals(MarkdownParse.getLinks(content),linksList);
+
+    }
+
+    @Test
+    public void getLinksTestFile3() throws IOException{
+        //create list of answer links
+        List<String> linksList = List.of();
+
+        //get the file data, read it as a string and passs it into the markdown parse function
+        Path fileName = Path.of("test-file3.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content),linksList);
+
+    }
+
+    @Test
+    public void getLinksTestNewFile4() throws IOException{
+        //create list of answer links
+        List<String> linksList = List.of();
+
+        //get the file data, read it as a string and passs it into the markdown parse function
+        Path fileName = Path.of("test-file4.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content),linksList);
+
+    }
+
+    @Test
+    public void getLinksTestNewFile5() throws IOException{
+        //create list of answer links
+        List<String> linksList = List.of();
+
+        //get the file data, read it as a string and passs it into the markdown parse function
+        Path fileName = Path.of("test-file5.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content),linksList);
+
+    }
+
+    @Test
+    public void getLinksTestNewFile6() throws IOException{
+        //create list of answer links
+        List<String> linksList = List.of();
+
+        //get the file data, read it as a string and passs it into the markdown parse function
+        Path fileName = Path.of("test-file6.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content),linksList);
+
+    }
+    @Test
+    public void getLinksTestNewFile7() throws IOException{
+        //create list of answer links
+        List<String> linksList = List.of();
+
+        //get the file data, read it as a string and passs it into the markdown parse function
+        Path fileName = Path.of("test-file7.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content),linksList);
+    }
+
+    @Test
+    public void getLinksTestNewFile8() throws IOException{
+        //create list of answer links
+        List<String> linksList = List.of("a link on the first line");
+
+        //get the file data, read it as a string and passs it into the markdown parse function
+        Path fileName = Path.of("test-file8.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content),linksList);
+    }
+
+    @Test
+    public void getLinksTestNewFile9() throws IOException{
+        //create list of answer links
+        List<String> linksList = List.of();
+
+        //get the file data, read it as a string and passs it into the markdown parse function
+        Path fileName = Path.of("test-file9.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content),linksList);
     }
 }
